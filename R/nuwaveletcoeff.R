9d82e4d834f02cff03b23d71c01b29e5d83fe8da
Nick McKay <nickmckay@users.noreply.github.com>
2018-12-19 09:32:49 -0700

    add sigma as a variable input to nuwaveletcoeff

--- R/nuwaveletcoeff.R
+++ R/nuwaveletcoeff.R
@@ -1,7 +1,7 @@
 #'@export
 "nuwaveletcoeff" <-
-function(X, Y, t, o, wgt=cubicwgt, wgtrad=1)
-{   so <- 0.05*o
+function(X, Y, t, o, wgt=cubicwgt, wgtrad=1, sigma = 0.05)
+{   so <- sigma*o
     rx <- subset(X, abs(X-t)*so<wgtrad)
     ry <- subset(Y, abs(X-t)*so<wgtrad)
     s <- sum(wgt((X-t)*so))
